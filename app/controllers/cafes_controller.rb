8ba3260c687a1823f9cc9997bd5ee356b274d3cc
deliaconstantino <dconstan92@gmail.com>
2021-03-15 14:00:46 -0400

    added cafe validations and updated new cafe form

--- app/controllers/cafes_controller.rb
+++ app/controllers/cafes_controller.rb
@@ -1,5 +1,6 @@
 class CafesController < ApplicationController
   def index
+    @cafes = Cafe.all
   end
 
   def new
@@ -7,9 +8,22 @@ class CafesController < ApplicationController
   end
 
   def create
-    raise params.inspect
+    @cafe = Cafe.new(cafe_params)
+    if @cafe.save
+      redirect_to cafe_path(@cafe)
+    else
+      flash.now[:message] = @cafe.errors.full_messages
+      render :new
+    end
   end
 
   def show
+    @cafe = Cafe.find_by(id: params[:id])
+  end
+
+  private
+
+  def cafe_params
+    params.require(:cafe).permit(:name, :city, :state, :speciality, :table_chair_seating, :cushioned_seating, :outdoor_seating, :wifi, :food, :open_hour, :open_minute, :close_hour, :close_minute)
   end
 end
--- app/models/cafe.rb
+++ app/models/cafe.rb
@@ -3,4 +3,13 @@ class Cafe < ApplicationRecord
   has_many :users, through: :reviews
   has_many :cafe_categories
   has_many :categories, through: :cafe_categories
+
+  validates :name, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+
+  validates :open_hour, presence: true
+  validates :open_minute, presence: true
+  validates :close_hour, presence: true
+  validates :close_minute, presence: true
 end
